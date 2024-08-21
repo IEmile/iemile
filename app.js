b2b388475bad0a6fa7bd352756616194fe6f1c3e
IEmile <iemiledev@gmail.com>
2024-08-21 17:33:58 +0200

    Creating my project by adding my root files app.js (/)index.ejs. i also created the header

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const {render} = require('ejs');
+
+
+
+app.use(express.static("./static"));
+app.set('view engine','ejs');
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("eBaruwa.com");
+})
+
+
+app.get('/', (req,res)=>{
+    res.render("index")
+})
